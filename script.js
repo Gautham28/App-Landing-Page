6eee6850d1b486178fbf80c6adce18d857852c6c
Gautham28 <109268786+Gautham28@users.noreply.github.com>
2024-03-22 03:04:32 +0530

    1
    1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+
+    window.location.href = 'https://drive.google.com/file/d/1tP-RjMawodXQWaXy-MnZsQNYZcN5Ul1W/view?usp=drive_link';
+});
